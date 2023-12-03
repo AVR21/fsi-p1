390cc942eee276ddc8f12f6057ac5adf17e6a1f5
AVR21 <alejandroviera12@gmail.com>
2023-12-03 11:43:55 +0000

    test: added 4 new tests

--- run.py
+++ run.py
@@ -2,8 +2,24 @@
 
 import search
 
+
+
 ab = search.GPSProblem('A', 'B'
                        , search.romania)
+
+#Batería de tests
+
+oe = search.GPSProblem('O','E', 
+                       search.romania)
+
+gz = search.GPSProblem('G','Z', 
+                       search.romania)
+
+nd = search.GPSProblem('N','D', 
+                       search.romania)
+
+mf = search.GPSProblem('M','F', 
+                       search.romania)
 
 print(search.breadth_first_graph_search(ab).path())
 print(search.depth_first_graph_search(ab).path())
